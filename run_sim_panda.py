b0ac08b8efaf1f10a29a220e70849e37e5c27345
Kamalu <pearl.kamalu.cw@jhuapl.edu>
2026-06-30 09:49:29 -0400

    Updated Files for Simulation Setup
    NOTES:
    - MODIFIED: requirements.txt. Was missing the pyyaml package which was called into the script. Updated the requirements.txt list.
    - MODIFIED: .gitignore. VS Code files were getting generated. Needed to exclude them from the repo.
    - MODIFIED: run_sim_panda.py. This main file has my linter yelling at me. I had to fix it.
    
    	modified:   .gitignore
    	modified:   requirements.txt
    	modified:   run_sim_panda.py
    	modified:   README.md

--- run_sim_panda.py
+++ run_sim_panda.py
@@ -1,14 +1,14 @@
-import argparse
-from sim import print_helpers as ph
-from sim.World.sim_world import WORLD
 from pathlib import Path
+import argparse
+from sim.World.sim_world import WORLD
 
 def main():
+    """The main function that runs the simulation."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--config",
         help="yaml file to load settings from",
-        default="config\scene\mountain_range\scene.yaml",
+        default="config\\scene\\mountain_range\\scene.yaml",
     )
     parser.add_argument("--output_dir", help="path to output directory", default="logs")
     parser.add_argument("--n", help="number of trials", default=1, type=int)
@@ -20,7 +20,12 @@ def main():
     run(app=app)
 
 def run(app):
+    """
+    Actually runs the app. 
+    TODO: Put this function into its own function that make that file what we call to run the simulation.
+    """
     app.run()
+
 
 if __name__ == "__main__":
     main()
