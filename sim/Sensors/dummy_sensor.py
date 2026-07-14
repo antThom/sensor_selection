868f9445e0da45dee5640137ed7c6ffaca3001cd
Kamalu <pearl.kamalu.cw@jhuapl.edu>
2026-07-14 11:23:00 -0400

    Refactored Sensor Abstract Class
    NOTES:
    Mostly commented out unused code and added attributes needed by other classes.
    - MODIFIED: dummy_sensor.py. Moved a function into the Sensor abstract class
    - MODIFIED: sensor.py. Commented out unused code and added new attributes

--- sim/Sensors/dummy_sensor.py
+++ sim/Sensors/dummy_sensor.py
@@ -3,7 +3,7 @@
 from sim.sensors.sensor import Sensor, SensorType
 from sim.utils.functions import set_attr_from_configuration
 
-class DummySensor():
+class DummySensor(Sensor):
     """_summary_
     A dummy sensor that contains all the things to make a sensor... but no special functions
     
@@ -19,12 +19,5 @@ class DummySensor():
         self.type = SensorType.DUMMY
         self.name = None
     
-    def set_attributes(self, config:dict) -> None:
-        """_summary_
-        Overload of initalization created with loading it's own attributes
-        Args:
-            config (dict): _description_
-        """
-        
-        set_attr_from_configuration(self, config)
+
         
